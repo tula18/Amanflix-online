3670da8475efca81f18fdd104426f10a1487678f
Almog Cohen <ricohen65@gmail.com>
2026-04-21 21:11:35 +0300

    fix: Update API_URL to correct server address

--- repo/src/config.js
+++ repo/src/config.js
@@ -1,5 +1,5 @@
 //  export const API_URL = 'http://127.0.0.1:5001';
- export const API_URL = 'http://192.168.68.103:5001';
+ export const API_URL = 'http://192.168.68.111:5001';
 // export const API_URL = 'http://55.45.250.91:5001';
 export const ENV_TYPE = 'prod';
 export const DEV_IP = '192.168.68.119';
